48ceee0d9fa6674a8ee50435850bbe8853bdb8ba
Kevin Bao <kevinbao@Kevins-MacBook-Pro-4.local>
2025-06-09 18:08:21 -0700

    moved constants

--- Passionfruit/config/FirebaseConfig.tsx
+++ Passionfruit/config/FirebaseConfig.tsx
@@ -5,17 +5,11 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { firebaseConfig } from "@/constants";
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDtNBUU7lfC86zTSilMqbmnA9Lj7F_04O8",
-  authDomain: "passionfruit-3422a.firebaseapp.com",
-  projectId: "passionfruit-3422a",
-  storageBucket: "passionfruit-3422a.firebasestorage.app",
-  messagingSenderId: "18083696070",
-  appId: "1:18083696070:web:cb6010ccd62bdbe794b6c7",
-  measurementId: "G-4Y756W4GL1"
-};
+const firebaseConf = { ...firebaseConfig}
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig)
